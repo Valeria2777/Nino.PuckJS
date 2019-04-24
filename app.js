eaa47ab755fc6ea31704ba176b3d0a2578442aa7
Valeria2777 <valeria_avilagrajeola@hotmail.com>
2019-04-23 22:23:27 -0600

    add sql server configuration

--- app.js
+++ app.js
@@ -69,115 +69,133 @@
 
     app.set('port', process.env.PORT || 3000);
 
+    const knex = require('knex')({
+        client: 'mssql',
+        connection: {
+            server : '127.0.0.1',
+            user : 'sa',
+            password : 'Tenco1234',
+            database : 'PuckJS',
+        }
+    });
+
+    
+
+
     var server = app.listen(app.get('port'), function () {
-        debug('Express server listening on port ' + server.address().port);
+        console.log('Express server listening on port ' + server.address().port);
+        knex.select().from('Device').then(res => {
+            console.log(res);
+        }).catch(e => {
+            console.log(e);
+        })
     });
 
-    /*Start the scan of devices.
-     */
-    noble.on('stateChange', function (state) {
-        console.log("Noble: stateChange -> " + state);
-        if (state === "poweredOn")
-            noble.startScanning([], true);
-    });
-    /*Search the puck.js device, and send the command.
-    */
-    noble.on('discover', function (dev) {
-        console.log("Found device: ", dev.address);
-        if (dev.address !== address) {
-            return;
-        }
-         noble.stopScanning();
-        connect(dev, function () {
-            // Connected!
-            write(command, function () {
-                disconnect();
-            });
-        });
-    });
-    /*
-     *Performs the search for devices with certain features to connect.
-     * @param {any} dev: is the device.
-     * @param {any} callback
-     */
-    function connect(dev, callback) {
-        btDevice = dev;
-        console.log("BT> Connecting");
-        btDevice.on('disconnect', function () {
-            console.log("Disconnected");
-        });
-        btDevice.connect(function (error) {
-            if (error) {
-                console.log("BT> ERROR Connecting", error);
-                btDevice = undefined;
-                return;
-            }
-            console.log("BT> Connected");
-            btDevice.discoverAllServicesAndCharacteristics(function (error, services, characteristics) {
-                function findByUUID(list, uuid) {
-                    for (var i = 0; i < list.length; i++)
-                        if (list[i].uuid === uuid) {
-                            return list[i];
-                        }
-                    return undefined;
-                }
+    // /*Start the scan of devices.
+    //  */
+    // noble.on('stateChange', function (state) {
+    //     console.log("Noble: stateChange -> " + state);
+    //     if (state === "poweredOn")
+    //         noble.startScanning([], true);
+    // });
+    // /*Search the puck.js device, and send the command.
+    // */
+    // noble.on('discover', function (dev) {
+    //     console.log("Found device: ", dev.address);
+    //     if (dev.address !== address) {
+    //         return;
+    //     }
+    //      noble.stopScanning();
+    //     connect(dev, function () {
+    //         // Connected!
+    //         write(command, function () {
+    //             disconnect();
+    //         });
+    //     });
+    // });
+    // /*
+    //  *Performs the search for devices with certain features to connect.
+    //  * @param {any} dev: is the device.
+    //  * @param {any} callback
+    //  */
+    // function connect(dev, callback) {
+    //     btDevice = dev;
+    //     console.log("BT> Connecting");
+    //     btDevice.on('disconnect', function () {
+    //         console.log("Disconnected");
+    //     });
+    //     btDevice.connect(function (error) {
+    //         if (error) {
+    //             console.log("BT> ERROR Connecting", error);
+    //             btDevice = undefined;
+    //             return;
+    //         }
+    //         console.log("BT> Connected");
+    //         btDevice.discoverAllServicesAndCharacteristics(function (error, services, characteristics) {
+    //             function findByUUID(list, uuid) {
+    //                 for (var i = 0; i < list.length; i++)
+    //                     if (list[i].uuid === uuid) {
+    //                         return list[i];
+    //                     }
+    //                 return undefined;
+    //             }
 
-                var btUARTService = findByUUID(services, "6e400001b5a3f393e0a9e50e24dcca9e");
-                txCharacteristic = findByUUID(characteristics, "6e400002b5a3f393e0a9e50e24dcca9e");
-                rxCharacteristic = findByUUID(characteristics, "6e400003b5a3f393e0a9e50e24dcca9e");
-                if (error || !btUARTService || !txCharacteristic || !rxCharacteristic) {
-                    console.log("BT> ERROR getting services/characteristics");
-                    console.log("Service " + btUARTService);
-                    console.log("TX " + txCharacteristic);
-                    console.log("RX " + rxCharacteristic);
-                    btDevice.disconnect();
-                    txCharacteristic = undefined;
-                    rxCharacteristic = undefined;
-                    btDevice = undefined;
-                    return openCallback();
-                }
+    //             var btUARTService = findByUUID(services, "6e400001b5a3f393e0a9e50e24dcca9e");
+    //             txCharacteristic = findByUUID(characteristics, "6e400002b5a3f393e0a9e50e24dcca9e");
+    //             rxCharacteristic = findByUUID(characteristics, "6e400003b5a3f393e0a9e50e24dcca9e");
+    //             if (error || !btUARTService || !txCharacteristic || !rxCharacteristic) {
+    //                 console.log("BT> ERROR getting services/characteristics");
+    //                 console.log("Service " + btUARTService);
+    //                 console.log("TX " + txCharacteristic);
+    //                 console.log("RX " + rxCharacteristic);
+    //                 btDevice.disconnect();
+    //                 txCharacteristic = undefined;
+    //                 rxCharacteristic = undefined;
+    //                 btDevice = undefined;
+    //                 return openCallback();
+    //             }
 
-                rxCharacteristic.on('data', function (data) {
-                    var s = "";
-                    for (var i = 0; i < data.length; i++) {
-                        s += String.fromCharCode(data[i]);
-                    }
-                    console.log("Received", JSON.stringify(s));
-                });
-                rxCharacteristic.subscribe(function () {
-                    callback();
-                });
-            });
-        });
-    };
-    /*
-     * Write the obtained data.
-     * @param {any} data
-     * @param {any} callback
-     */
-    function write(data, callback) {
-        function writeAgain() {
-            if (!data.length) {
-                return callback();
-            }
-            var d = data.substr(0, 20);
-            data = data.substr(20);
-            var buf = new Buffer(d.length);
-            for (var i = 0; i < buf.length; i++) {
-                buf.writeUInt8(d.charCodeAt(i), i);
-            }
-                txCharacteristic.write(buf, false, writeAgain);
-        }
-        writeAgain();
-    }
-    /**Dissconect the devices.
-     **/
-    function disconnect() {
-        btDevice.disconnect();
-    }
-    var API = {
-        connect: connect,
-        write: write
-    };
-    return API;
+    //             rxCharacteristic.on('data', function (data) {
+    //                 var s = "";
+    //                 for (var i = 0; i < data.length; i++) {
+    //                     s += String.fromCharCode(data[i]);
+    //                 }
+    //                 console.log("Received", JSON.stringify(s));
+    //             });
+    //             rxCharacteristic.subscribe(function () {
+    //                 callback();
+    //             });
+    //         });
+    //     });
+    // };
+    // /*
+    //  * Write the obtained data.
+    //  * @param {any} data
+    //  * @param {any} callback
+    //  */
+    // function write(data, callback) {
+    //     function writeAgain() {
+    //         if (!data.length) {
+    //             return callback();
+    //         }
+    //         var d = data.substr(0, 20);
+    //         data = data.substr(20);
+    //         var buf = new Buffer(d.length);
+    //         for (var i = 0; i < buf.length; i++) {
+    //             buf.writeUInt8(d.charCodeAt(i), i);
+    //         }
+    //             txCharacteristic.write(buf, false, writeAgain);
+    //     }
+    //     writeAgain();
+    // }
+    // /**Dissconect the devices.
+    //  **/
+    // function disconnect() {
+    //     btDevice.disconnect();
+    // }
+    // var API = {
+    //     connect: connect,
+    //     write: write
+    // };
+    // return API;
 })();
